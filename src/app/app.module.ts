f21c3a6ebe3357b1b48e3d61878292d18f691c16
Jaime Zapata <jezg1992@hotmail.com>
2018-02-16 17:18:01 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +12,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({uri: ''}),
+      cache: new InMemoryCache()
+    });
+  }
+ }
